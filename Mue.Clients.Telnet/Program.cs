a3a384adfa00130548e549b1e5fb9f6772c018e1
Kauko <kauko@biosynth.link>
2024-02-22 23:49:02 -0800

    Add missing telnet server target env var

--- Mue.Clients.Telnet/Program.cs
+++ Mue.Clients.Telnet/Program.cs
@@ -2,6 +2,9 @@
 
 var cts = new CancellationTokenSource();
 
-var telnetClient = new TelnetClient("http://localhost:5000/mueclient");
+var backendServerRoot = Environment.GetEnvironmentVariable("BACKEND_SERVER_URL") ?? "http://localhost:5000";
+var backendServerPath = $"{backendServerRoot}/mueclient";
+
+var telnetClient = new TelnetClient(backendServerPath);
 Console.WriteLine("Listening on port 8888");
 await telnetClient.StartListening(8888, cts);
